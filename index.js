92b29e017e033b09801ca2020845bc233a5b78ef
Craig Ferreira <ferreira.craig@gmail.com>
2021-02-05 20:09:37 -0500

    made changes

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
+let lists=document.getElementById('lists')
 let first= document.getElementById('list1');
 let second= document.getElementById('list2');
 let third= document.getElementById('list3')
@@ -27,8 +28,14 @@ function createPeople(){
     let currlist;
     for(let val of users){
        currList= document.getElementById(val.slot)
-       currList.innerHTML+=`<div class='list-info' style='height:100px; width:200px; border:1px solid orange;'>${val.name}</div>`
-
+    //    currList.innerHTML+=`<div class='list-info' style='height:100px; width:200px; border:1px solid orange;'>${val.name}</div>`
+        let currEl=document.createElement('div');
+        currEl.innerText=val.name;
+        currEl.classList='list-info';
+        currList.appendChild(currEl)
+        // currEl.classList='list-info';
+        // currEl.style='height:100px; width:200px; border:1px solid orange;'
+        // currlist.appendChild(currEl)
     }
 }
 
@@ -37,7 +44,7 @@ let moveFrom=null;
 let moveTo=null;
 let divsToMove=[];
 
-document.addEventListener('click', (e)=>{
+lists.addEventListener('click', (e)=>{
     if(e.target.className==='list-info') {
         divToMove=e.target;
         divToMove.style.background='red';
@@ -46,12 +53,14 @@ document.addEventListener('click', (e)=>{
 
     else if(e.target.className==='move-left'){
         for(let div of divsToMove){
+            if(e.target.parentElement.parentElement===div.parentElement){
             moveFrom=div.parentElement;
             moveTo=div.parentElement.previousElementSibling
             moveFrom.removeChild(div);
             moveTo.appendChild(div);
             div.display='inline'
             div.style.background='white';
+            }
         }
         divsToMove=[];
         
@@ -59,12 +68,14 @@ document.addEventListener('click', (e)=>{
 
         else if (e.target.className==='move-right'){
         for(let div of divsToMove){
+            if(e.target.parentElement.parentElement===div.parentElement){
             moveFrom=div.parentElement;
             moveTo=div.parentElement.nextElementSibling
             moveFrom.removeChild(div);
             moveTo.appendChild(div);
             div.display='inline'
             div.style.background='white';
+            }
         }
         divsToMove=[];
     }
